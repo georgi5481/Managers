9d5da8541ed7ba3d65401c09b3c5a007772577f1
George <georgi5481@abv.bg>
2022-08-23 22:20:15 +0300

    1 new class for drawing using the manager

---
+++ manager_utils/include/manager_utils/drawing/Widget.h
@@ -0,0 +1,16 @@
+#ifndef MANAGER_UTILS_INCLUDE_MANAGER_UTILS_DRAWING_WIDGET_H_
+#define MANAGER_UTILS_INCLUDE_MANAGER_UTILS_DRAWING_WIDGET_H_
+
+//C system includes
+
+//C++ system includes
+
+//3rd-party includes
+
+//Own includes
+
+//Forward Declaration
+
+
+
+#endif /* MANAGER_UTILS_INCLUDE_MANAGER_UTILS_DRAWING_WIDGET_H_ */
